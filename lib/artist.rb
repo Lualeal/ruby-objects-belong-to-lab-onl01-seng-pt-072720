ad1a35b42caf0dd33cbc0b8f5f3de196a7d8c247
Luana Leal <luana.leal.eq@gmail.com>
2020-08-26 03:20:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist 
-  attr_accessor :name, :title
+  attr_accessor :artist, :title
   
-  def initialize (name, title)
+  def initialize (artist, title)
     @name = name
     @title = title
   end 
